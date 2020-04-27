04adb39f79b1e15ec697be2b755f339db0055231
Kelsey Sparkman <kelseysparkman14@gmail.com>
2020-04-27 16:22:44 -0600

    start shoppingListController, create method signatures

---
+++ Meal-Prep-Sunday/Meal-Prep-Sunday/Models/ShoppingListController.swift
@@ -0,0 +1,32 @@
+//
+//  ShoppingListController.swift
+//  Meal-Prep-Sunday
+//
+//  Created by Kelsey Sparkman on 4/27/20.
+//  Copyright © 2020 Turtle. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingListController {
+    
+    // Mark: - Properties
+    
+    // Mark: - Crud Functions
+    func create() {
+        
+    }
+    
+    func read() {
+        
+    }
+    
+    func update() {
+        
+    }
+    
+    func delete() {
+        
+    }
+    
+}//End of class
