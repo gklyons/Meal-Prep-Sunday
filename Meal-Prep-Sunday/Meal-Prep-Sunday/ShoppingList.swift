b1aa547ddb581102d531d1d8531ec4489ec6a876
Kelsey Sparkman <kelseysparkman14@gmail.com>
2020-04-27 11:36:06 -0600

    create shopping list model

---
+++ Meal-Prep-Sunday/Meal-Prep-Sunday/ShoppingList.swift
@@ -0,0 +1,20 @@
+//
+//  Meal-Prep-Sunday.swift
+//  Meal-Prep-Sunday
+//
+//  Created by Kelsey Sparkman on 4/27/20.
+//  Copyright © 2020 Turtle. All rights reserved.
+//
+
+import Foundation
+
+class ShoppingList {
+    
+    let ingredient: String
+    let measurement: String
+    
+    init(ingredient: String, measurement: String) {
+        self.ingredient = ingredient
+        self.measurement = measurement
+    }
+}
