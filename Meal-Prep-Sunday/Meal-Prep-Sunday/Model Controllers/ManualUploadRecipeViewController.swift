6acf206f71472cc2178b49c468b4410dc3fa8aa6
Kelsey Sparkman <kelseysparkman14@gmail.com>
2020-05-04 12:32:35 -0600

    refactor storyboards, create manualUploadVC

---
+++ Meal-Prep-Sunday/Meal-Prep-Sunday/Model Controllers/ManualUploadRecipeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ManualUploadRecipeViewController.swift
+//  Meal-Prep-Sunday
+//
+//  Created by Kelsey Sparkman on 5/4/20.
+//  Copyright © 2020 Turtle. All rights reserved.
+//
+
+import UIKit
+
+class ManualUploadRecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
