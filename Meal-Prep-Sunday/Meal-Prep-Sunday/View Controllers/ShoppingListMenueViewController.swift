b12dd9c7767443fac301d13bf734af568aa35243
Jakob Haslam <jakehaslam38@gmail.com>
2020-05-08 16:58:48 -0600

    Rebuilding 11am-5pm

---
+++ Meal-Prep-Sunday/Meal-Prep-Sunday/View Controllers/ShoppingListMenueViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShoppingListMenueViewController.swift
+//  Meal-Prep-Sunday
+//
+//  Created by Jake Haslam on 5/8/20.
+//  Copyright © 2020 Turtle. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListMenueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Meal-Prep-Sunday/Meal-Prep-Sunday/View Controllers/ShoppingListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShoppingListTableViewCell.swift
+//  Meal-Prep-Sunday
+//
+//  Created by Jake Haslam on 5/8/20.
+//  Copyright © 2020 Turtle. All rights reserved.
+//
+
+import UIKit
+
+class ShoppingListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
