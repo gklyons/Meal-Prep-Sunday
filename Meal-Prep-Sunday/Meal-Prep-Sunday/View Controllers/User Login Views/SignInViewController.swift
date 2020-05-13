16c254b1ac8cef48e8d8fa72ce8a2ccc3d1bd805
Kelsey Sparkman <kelseysparkman14@gmail.com>
2020-05-13 12:05:38 -0600

    login views complete. Send password reset working

--- Meal-Prep-Sunday/Meal-Prep-Sunday/View Controllers/User Login Views/SignInViewController.swift
+++ Meal-Prep-Sunday/Meal-Prep-Sunday/View Controllers/User Login Views/SignInViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignInViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        errorLabel.isHidden = true
     }
-
+    
+    @IBAction func signInButtonTapped(_ sender: Any) {
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                self.errorLabel.isHidden = false
+                print(error, error.localizedDescription)
+            } else {
+                self.transitionToHome()
+            }
+        }
+    }
+    
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func transitionToHome() {
+        performSegue(withIdentifier: "toTabBarController", sender: self)
+    }
 }
